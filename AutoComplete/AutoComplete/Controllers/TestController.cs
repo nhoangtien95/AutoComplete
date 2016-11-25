717148d91474a940631929448370e2362776495c
nhoangtien95 <nhoangtien95@gmail.com>
2016-11-25 23:40:26 +0700

    25/11/2016 AutoComplete

---
+++ AutoComplete/AutoComplete/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoComplete.Models;
+
+namespace AutoComplete.Controllers
+{
+    public class TestController : Controller
+    {
+        private ShopMyPhamEntities db = new ShopMyPhamEntities();
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult test(string term)
+        {
+            List<SanPham> list;
+            list = db.SanPhams.Include("SanPhamHinhs").Where(x => x.TenSanPham.Contains(term)).ToList();
+                    
+            return Json(list.Select(x => new { TenSanPham = x.TenSanPham, TenHinh = x.SanPhamHinhs.FirstOrDefault().TenHinh}),JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
